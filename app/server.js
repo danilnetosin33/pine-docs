e1a225c3ded07a6d3e0cfae5b4223688ad823632
danilnetosin33 <danilnetosin@gmail.com>
2023-04-20 20:46:51 +0300

    qqqqq

--- app/server.js
+++ app/server.js
@@ -2,10 +2,12 @@ const https = require("https");
 const http = require("http");
 
 //APP
+const bodyParser = require("body-parser");
 var express = require("express"),
   app = express(),
   port = process.env.PORT || 80;
 app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.listen(port);
 //TRADINGECONOMICS
 const te = require("tradingeconomics");
@@ -13,17 +15,179 @@ te.login("65b105e712d343e:x0blzjyj39tnysh");
 //TELEGRAM
 const { Telegraf, Scenes, session } = require("telegraf");
 const Calendar = require("telegraf-calendar-telegram");
+const { MenuTemplate, MenuMiddleware } = require("telegraf-inline-menu");
 const { toHTML, toMarkdownV2 } = require("@telegraf/entity");
 const bot = new Telegraf("6271843557:AAEQ6ifCSa5En8lmtWvJ3kPYU-cTPYK6XmE");
+bot.use(session());
+
+function strFromDate(date) {
+  return date.toISOString().split("T")[0];
+}
+
+global["calendar_query"] = {
+  importance: 1,
+  initDate: strFromDate(new Date()),
+  endDate: strFromDate(new Date()),
+};
 
 const botCalendar = new Calendar(bot);
+let datesCount = 0;
+botCalendar.setDateListener((context, date) => {
+  if (datesCount == 0) {
+    global["calendar_query"].initDate = date;
+    context.reply("Start Date: " + date);
+  }
+  if (datesCount == 1) {
+    global["calendar_query"].endDate = date;
+    context.reply("End Date: " + date);
+    context.reply("----------------------------");
+    menuMiddleware.replyToContext(context);
+  }
+  datesCount += 1;
+});
+const calendarMenu = new MenuTemplate((ctx) => {
+  if (!ctx.session) {
+    ctx.session = {
+      calendar: {
+        importance: "⭐️ (Low)",
+        dates: "Today",
+      },
+    };
+  }
+  return "Calendar: ";
+});
+//Importance
+const importanceSubmenu = new MenuTemplate((ctx) => {
+  if (!ctx.session) {
+    ctx.session = {};
+  }
+  return `Importance:`;
+});
+importanceSubmenu.choose(
+  "importance",
+  ["⭐️ (Low)", "⭐️⭐️ (Medium)", "⭐️⭐️⭐️ (High)"],
+  {
+    columns: 1,
+    maxRows: 3,
+    buttonText: (ctx, text) => {
+      if (text == ctx.session.calendar.importance) {
+        return text + " ☑️";
+      }
+      return text;
+    },
+    do: (ctx, key) => {
+      ctx.session.calendar.importance = key;
+      global["calendar_query"].importance = key.length / 2;
+      console.log(key.length, global["calendar_query"].importance);
+      return "..";
+    },
+  }
+);
+importanceSubmenu.interact("<= Back", "back_importance", {
+  do: async (ctx) => {
+    return "..";
+  },
+});
+calendarMenu.submenu(
+  (ctx) => `Importance: ${ctx.session.calendar.importance} `,
+  "sub1",
+  importanceSubmenu
+);
+//Dates
+const datesSubmenu = new MenuTemplate((ctx) => {
+  if (!ctx.session) {
+    ctx.session = {};
+  }
+  return `Dates:  `;
+});
+datesSubmenu.choose(
+  "dates",
+  ["Today", "Tomorrow", "Next Week", "Next Month", "Custom"],
+  {
+    columns: 2,
+    maxRows: 3,
+    do: (ctx, key, newState) => {
+      ctx.session.calendar.dates = key;
+      let today = new Date();
+      let start_date = today;
+      let end_date = null;
 
-botCalendar.setDateListener((context, date) => context.reply(date));
-// retreive the calendar HTML
-bot.command("calendar1", (context) =>
-  context.reply("Here you are", botCalendar.getCalendar())
+      if (key.toLowerCase() != "custom") {
+        if (key.toLowerCase() == "today") {
+          end_date = today;
+        }
+        if (key.toLowerCase() == "tomorrow") {
+          let tomorrow = new Date(today);
+          end_date = new Date(tomorrow.setDate(tomorrow.getDate() + 1));
+        }
+        if (key.toLowerCase() == "next week") {
+          let next_week = new Date(today);
+          end_date = new Date(next_week.setDate(next_week.getDate() + 7));
+        }
+        if (key.toLowerCase() == "next month") {
+          let next_month = new Date(today);
+          end_date = new Date(next_month.setMonth(next_month.getMonth() + 1));
+        }
+        global["calendar_query"].initDate = strFromDate(start_date);
+        global["calendar_query"].endDate = strFromDate(end_date);
+      } else {
+        ctx.reply("Select start/end dates:", botCalendar.getCalendar());
+      }
+
+      return "..";
+    },
+    buttonText: (ctx, text) => {
+      if (text == ctx.session.calendar.dates) {
+        return text + " ☑️";
+      }
+      return text;
+    },
+  }
+);
+datesSubmenu.interact("<= Back", "back_dates", {
+  do: async (ctx) => {
+    return "..";
+  },
+});
+calendarMenu.submenu(
+  (ctx) => {
+    return `Dates: (${global["calendar_query"].initDate} - ${global["calendar_query"].endDate})`;
+  },
+  "sub2",
+  datesSubmenu
 );
 
+// Request Calendar
+calendarMenu.interact("Get Calendar", "get_calendar", {
+  do: async (ctx) => {
+    console.log("SADASD", te);
+    await te
+      .getCalendar(
+        (importance = global["calendar_query"].importance.toString()),
+        (start_date = global["calendar_query"].initDate),
+        (end_date = global["calendar_query"].endDate)
+      )
+      .then((data) => {
+        ctx.reply(`LENGTH :: ${data.length}`);
+        return data;
+      })
+      .then((data) => {
+        data.forEach((element) => {
+          let message = "";
+          for (let prop in element)
+            message += ` ${prop} : ${element[prop]}  \n  `;
+          ctx.reply(toHTML({ text: message }));
+        });
+        return data;
+      });
+    return false;
+  },
+});
+const menuMiddleware = new MenuMiddleware("/", calendarMenu);
+bot.command("calendar", (ctx) => menuMiddleware.replyToContext(ctx));
+bot.use(menuMiddleware);
+
+// Scene
 const sceneCalendar = new Scenes.BaseScene("calendar");
 sceneCalendar.enter((ctx) => {
   global["Calendar"] = {
@@ -57,14 +221,14 @@ sceneCalendar.on("text", (ctx) => {
   }
   global["Calendar"].step += 1;
 });
-
 sceneCalendar.leave((ctx) => {
   ctx.reply(JSON.stringify(global.Calendar));
 });
 
 const stage = new Scenes.Stage([sceneCalendar]);
-bot.use(session());
+
 bot.use(stage.middleware());
+
 // Local API
 app.get("/", function (req, res) {
   console.log("RES", res);
@@ -82,7 +246,6 @@ app.post("/", function (req, res) {
       }
     );
   }
-
   res.sendStatus(200);
 });
 
@@ -93,60 +256,19 @@ app.get("/calendar", function (req, res) {
     ctx.reply(JSON.stringify(data));
   });
 });
-bot.command("calendar", (ctx) => {
-  data = te
-    .getCalendar()
-    .then((data) => {
-      ctx.reply(`LENGTH :: ${data.length}`);
-      return data;
-    })
-    .then(function (data) {
-      console.log("CALEDNAR_DATA", data);
-
-      data.forEach((element) => {
-        let message = "";
-        for (let prop in element)
-          message += ` ${prop} : ${element[prop]}  \n  `;
-        ctx.reply(toHTML({ text: message }));
-      });
-    });
-});
-
-bot.command("test", (ctx) => {
-  ctx.scene.enter("calendar");
-});
-
-// bot.command("calendar_dates", (ctx) => {
-//   ctx.scene.enter("importance");
-//   console.log(global.CalendarFilter);
-
-//   bot.on("text", (ctx) => {
-//     console.log(global.CalendarFilter);
-//     console.log("TEXTTTTTT");
-//     if (CalendarFilter.isClosed) {
-//       CalendarFilter.isClosed = false;
-//       ctx.reply(JSON.stringify(CalendarFilter));
-//     }
-//   });
-// });
 
 //Telegram API
 app.post("/telegram_test", function (req, res) {
-  bot.telegram.getChat("-1001967555467").then((res) => {
-    console.log("RESPONSE", res);
-  });
   bot.telegram.sendMessage("-1001967555467", "Hello WORLD");
   res.sendStatus(200);
 });
 
-bot.start((ctx) => {
-  // pine-script channel ID = -1001967555467
-  ctx.telegram.sendMessage("-1001967555467", "Hello");
+app.post("/bars_data", function (req, res) {
+  console.log("REQQQQQQ", req.body);
+  // console.log("RESSSSSS", res);
+  res.sendStatus(200);
 });
 
-bot.catch((err, ctx) => {
-  console.log(`Error:: ${ctx.updateType}`, err);
-});
 bot.launch();
 
 //Discord API
