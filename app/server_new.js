52e3b1b32d23b412f78ba6e3044b382924f9f5ad
danilnetosin33 <danilnetosin@gmail.com>
2023-05-04 19:55:23 +0300

    qqqq

--- app/server_new.js
+++ app/server_new.js
@@ -1,15 +1,24 @@
 const bodyParser = require("body-parser");
+const path = require("path");
 var express = require("express"),
   app = express(),
-  port = 5500;
+  port = 80;
 app.use(express.json());
 app.use(bodyParser.json({ extended: true }));
 app.listen(port);
+app.use(express.static(__dirname));
+app.use(express.static("assets"));
+app.use(express.static("frontend"));
 
 const calculateProfit = require("./pine_to_js_api");
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.sendFile(__dirname + "/frontend/index.html");
 });
+
+app.get("/hello", (req, res) => {
+  console.log("HELLO");
+});
+
 app.post("/calculate", function (req, res) {
   console.log("REQQQQQQQQq", req.body);
   calculateProfit("a", "b");
